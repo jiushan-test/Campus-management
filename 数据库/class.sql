5a22520570a620f8adb0280cefc71dc27f0785ce
旧衫 <40030236+jiushan-test@users.noreply.github.com>
2022-05-18 12:11:21 +0800

    Add files via upload

---
+++ 数据库/class.sql
@@ -0,0 +1,10 @@
+INSERT INTO `class` (`c_id`, `classes`) VALUES (223, '自动化4');
+INSERT INTO `class` (`c_id`, `classes`) VALUES (225, '自动化6');
+INSERT INTO `class` (`c_id`, `classes`) VALUES (226, '自动化7');
+INSERT INTO `class` (`c_id`, `classes`) VALUES (227, '自动化8');
+INSERT INTO `class` (`c_id`, `classes`) VALUES (283, '计算机科学与技术');
+INSERT INTO `class` (`c_id`, `classes`) VALUES (284, '计算机科学与技术1');
+INSERT INTO `class` (`c_id`, `classes`) VALUES (287, '计算机科学与技术2');
+INSERT INTO `class` (`c_id`, `classes`) VALUES (288, '计算机科学与技术3');
+INSERT INTO `class` (`c_id`, `classes`) VALUES (290, '自动化');
+INSERT INTO `class` (`c_id`, `classes`) VALUES (291, '控制工程');
